07abe8451bf9eec93ea3e3615bda091722f3aaef
coders rabbi <pcrabbi2020@gmail.com>
2023-06-09 23:03:48 +0600

    classes data loaded

--- src/Pages/Classes/SingleClass.jsx
+++ src/Pages/Classes/SingleClass.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const SingleClass = ({cls}) => {
-    const {_id, image, name, instractor} = cls;
+    const {_id, image, name, instructor_name, seats, price} = cls;
     console.log(cls);
     return (
         <div>
             <div>
                 <img src={image} alt="" />
-                
+                <h2>{name}</h2>
+                <h3>Instructor: {instructor_name}</h3>
+                <h3>Available Seats: {seats}</h3>
+                <p>Price: {price}</p>
             </div>
         </div>
     );
