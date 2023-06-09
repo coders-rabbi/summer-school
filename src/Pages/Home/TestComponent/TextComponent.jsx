9de0921d05525e8b1be15d567e0ba073af373c7c
coders rabbi <pcrabbi2020@gmail.com>
2023-06-09 19:14:11 +0600

    Popular instractor done

--- src/Pages/Home/TestComponent/TextComponent.jsx
+++ src/Pages/Home/TestComponent/TextComponent.jsx
@@ -5,15 +5,19 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import "./styles.css";
 import { EffectCoverflow, Pagination } from "swiper";
-
-import dance04 from "../../../assets/popular/inner_dance_04.jpg"
 import SectionTitle from "../../../SectionTitile/SectionTitle";
+import instructors01 from "../../../assets/Instructors/1677507598430.jpg"
+import instructors02 from "../../../assets/Instructors/1622725553860.jpg"
+import instructors03 from "../../../assets/Instructors/09-Govinda.jpg"
+import instructors04 from "../../../assets/Instructors/classical.jpg"
+import instructors05 from "../../../assets/Instructors/Nora-Fatehi-FIFA.png"
+import instructors06 from "../../../assets/Instructors/dancerSix.png"
 
 
 const TextComponent = () => {
     return (
         <div className='max-w-screen-xl mx-auto'>
-            <SectionTitle 
+            <SectionTitle
                 subHeading="Instructors" heading="Our popular instructors"
             ></SectionTitle>
             <Swiper
@@ -32,32 +36,137 @@ const TextComponent = () => {
                 modules={[EffectCoverflow, Pagination]}
                 className="mySwiper"
             >
-                <SwiperSlide>
-                    <img className="w-96" src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                <SwiperSlide className="overflow-hidden">
+                    <img className="h-[300px]" src={instructors01} />
+                    <div className="
+                    absolute
+                    overflow-hidden
+                    -bottom-32
+                    hover:bottom-0
+                    text-white
+                    w-full
+                    h-[300px]
+                    bg-gradient-to-t
+                    from-yellow-500
+                    flex
+                    flex-col
+                    justify-end
+                    items-center
+                    p-6
+                    ">
+                        <h3 className="text-2xl font-semibold font-josefin">Jhankar Mahabub</h3>
+                        <p className="text-black text-xl font-josefin">Total Students: 750+</p>
+                    </div>
                 </SwiperSlide>
-                <SwiperSlide>
-                    <img className="w-96" src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                <SwiperSlide className="overflow-hidden">
+                    <img className="h-[300px]" src={instructors02} alt="" />
+                    <div className="
+                    absolute
+                    overflow-hidden
+                    -bottom-32
+                    hover:bottom-0
+                    text-white
+                    w-full
+                    h-[300px]
+                    bg-gradient-to-t
+                    from-yellow-500
+                    flex
+                    flex-col
+                    justify-end
+                    items-center
+                    p-6
+                    ">
+                        <h3 className="text-2xl text-center font-semibold font-josefin">Sheikh Mahammad Rifat</h3>
+                        <p className="text-black text-xl font-josefin">Total Students: 750+</p>
+                    </div>
                 </SwiperSlide>
-                <SwiperSlide>
-                    <img className="w-96" src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                <SwiperSlide className="overflow-hidden">
+                    <img className="h-[300px]" src={instructors03} alt="" />
+                    <div className="
+                    absolute
+                    overflow-hidden
+                    -bottom-32
+                    hover:bottom-0
+                    text-white
+                    w-full
+                    h-[300px]
+                    bg-gradient-to-t
+                    from-yellow-500
+                    flex
+                    flex-col
+                    justify-end
+                    items-center
+                    p-6
+                    ">
+                        <h3 className="text-2xl font-semibold font-josefin">Govinda</h3>
+                        <p className="text-black text-xl font-josefin">Total Students: 750+</p>
+                    </div>
                 </SwiperSlide>
-                <SwiperSlide>
-                    <img className="w-96" src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                <SwiperSlide className="overflow-hidden">
+                    <img className="h-[300px]" src={instructors04} alt="" />
+                    <div className="
+                    absolute
+                    overflow-hidden
+                    -bottom-32
+                    hover:bottom-0
+                    text-white
+                    w-full
+                    h-[300px]
+                    bg-gradient-to-t
+                    from-yellow-500
+                    flex
+                    flex-col
+                    justify-end
+                    items-center
+                    p-6
+                    ">
+                        <h3 className="text-2xl font-semibold font-josefin">Susmita Sen</h3>
+                        <p className="text-black text-xl font-josefin">Total Students: 750+</p>
+                    </div>
                 </SwiperSlide>
-                <SwiperSlide>
-                    <img className="w-96" src="https://swiperjs.com/demos/images/nature-5.jpg" />
+                <SwiperSlide className="overflow-hidden">
+                    <img className="h-[300px]" src={instructors05} alt="" />
+                    <div className="
+                    absolute
+                    overflow-hidden
+                    -bottom-32
+                    hover:bottom-0
+                    text-white
+                    w-full
+                    h-[300px]
+                    bg-gradient-to-t
+                    from-yellow-500
+                    flex
+                    flex-col
+                    justify-end
+                    items-center
+                    p-6
+                    ">
+                        <h3 className="text-2xl font-semibold font-josefin">Nora Fatehi</h3>
+                        <p className="text-black text-xl font-josefin">Total Students: 750+</p>
+                    </div>
                 </SwiperSlide>
-                <SwiperSlide>
-                    <img className="w-96" src="https://swiperjs.com/demos/images/nature-6.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img className="w-96" src="https://swiperjs.com/demos/images/nature-7.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img className="w-96" src="https://swiperjs.com/demos/images/nature-8.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+                <SwiperSlide className="overflow-hidden">
+                    <img className="h-[300px]" src={instructors06} alt="" />
+                    <div className="
+                    absolute
+                    overflow-hidden
+                    -bottom-32
+                    hover:bottom-0
+                    text-white
+                    w-full
+                    h-[300px]
+                    bg-gradient-to-t
+                    from-yellow-500
+                    flex
+                    flex-col
+                    justify-end
+                    items-center
+                    p-6
+                    ">
+                        <h3 className="text-2xl font-semibold font-josefin">Daniela Blume</h3>
+                        <p className="text-black text-xl font-josefin">Total Students: 750+</p>
+                    </div>
                 </SwiperSlide>
             </Swiper>
 
--- src/SectionTitile/SectionTitle.jsx
+++ src/SectionTitile/SectionTitle.jsx
@@ -4,7 +4,7 @@ const SectionTitle = ({ subHeading, heading }) => {
     return (
         <div className='mb-10 flex flex-col items-center'>
             <p className='px-10 text-2xl border-b-4 border-b-[#D9D9D9] py-3 mb-10 text-[#D79C62]'>{subHeading}</p>
-            <p className=' text-5xl uppercase border-b-4 border-b-[D9D9D9] py-3 mt-3 px-20'>{heading}</p>
+            <p className=' text-5xl text-center uppercase border-b-4 border-b-[D9D9D9] py-3 mt-3 lg:px-20 md:px-10'>{heading}</p>
         </div>
     );
 };
