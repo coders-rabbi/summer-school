c077f19846170f5603b3be54fa70a7637487e751
coders rabbi <pcrabbi2020@gmail.com>
2023-06-13 01:10:28 +0600

    jwt token created using axios and post

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from "../../firebase.config";
+import axios from "axios";
 export const AuthContext = createContext();
 // const auth = getAuth(app);
 
@@ -41,8 +42,18 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (loggedUser) => {
             setUser(loggedUser);
+            console.log(loggedUser);
+
+            //get and set jwt token
+            if (loggedUser) {
+                axios.post('http://localhost:5000/jwt', { email: loggedUser.email })
+                .then(data => {
+                    console.log(data);
+                })
+            }
+
+
             setLoading(false);
-            // console.log(loggedUser);
         });
         return () => {
             return unsubscribe();
